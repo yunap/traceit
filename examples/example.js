782bfd10b877438d1bd935403eb92fe254d1b892
Yuna Portnoy <yunay@yahoo.com>
2014-05-31 20:06:20 -0700

    js-beautify examples/*

--- examples/example.js
+++ examples/example.js
@@ -1,88 +1,102 @@
+;
+(function($) {
+
+    window.exports = {
+        testing: function() {
+
+            $('#example_title').trace();
+            $('#example').trace({
+                isVisible: false
+            });
+            $('#example31').trace({
+                isVisible: false
+            });
+            $('#example32').trace({
+                isVisible: false
+            });
+
+        }
+    };
+
+    $("#trace_example").click(function(e) {
+
+        e.preventDefault();
+
+        //var inst =  $('#example').data('trace');
+        //inst.reTrace({ isVisible: true });
+        //or call:
+        $('#example').trigger('adjust.trace');
+
+    });
 
 
+    $("#trace_example2").click(function(e) {
 
-(function ($) {
-	
-	window.exports = { 
-		    testing: function () {
-		     
-		       $('#example_title').trace();
-		       $('#example').trace({ isVisible: false });
-		       $('#example31').trace({ isVisible: false });
-		       $('#example32').trace({ isVisible: false });
-	    
-		    }
-	};
-	
-	$("#trace_example").click(function(e){
-		
-		e.preventDefault();
-		
-		//var inst =  $('#example').data('trace');
-		//inst.reTrace({ isVisible: true });
-		//or call:
-		$('#example').trigger('adjust.trace');
-		 
-	});
-	
- 
-	$("#trace_example2").click(function(e){
-		
-		e.preventDefault();
-		
-		$('#example2').trace({ 
-			traceOpt : { 
-				'stroke': 'blue', 
-				'stroke-width': 2, 
-				'stroke-opacity': 1, 
-				'fill': '#00ff00', 
-				'fill-opacity': 0.2 
-			},
-			redrawSpeed : 6000,
-			trace_canvas_padding: 4
-		});		 
-	});
-	
-	
-    $("#trace_example3").click(function(e){
-		
-		e.preventDefault();
-		
-		var inst1 =  $('#example31').data('trace');
-		inst1.reTrace({ 'stroke': '#880000', 'stroke-width': 2, 'stroke-opacity': 1, 'isVisible' : 'true' });
+        e.preventDefault();
 
-		$('#example32').data('trace');
-		$('#example32').trigger(
-		{ 
-			type: 'adjust.trace', 
-			adjustments: 
-				{ 
-				'stroke': '#aa0000', 
-				'stroke-width': 4, 
-				'stroke-opacity': 1 
-				}
-		});	
-		 
-	});
-	
-	$("#trace_example4").click(function(e){
-		
-		e.preventDefault();
-		
-		$('#example4').trace(
-		{  
-		traceOpt : {  
-				'stroke-width': 4, 
-				'stroke-opacity': .5
-		}, 
-	        onClick : function( me ) { 
-							alert('triggered when user clicks on a trace shape.');
-							me.options.shape.animate({opacity: 0}, 1000, function(){ me.hideTrace();}); 
-						}, 
-			onHide : function(){ alert('triggered when hide animation completes.'); },
-			onEndTrace: function() { alert("triggered when trace animation completes."); },
-		});
-		 
-	});
-	
+        $('#example2').trace({
+            traceOpt: {
+                'stroke': 'blue',
+                'stroke-width': 2,
+                'stroke-opacity': 1,
+                'fill': '#00ff00',
+                'fill-opacity': 0.2
+            },
+            redrawSpeed: 6000,
+            trace_canvas_padding: 4
+        });
+    });
+
+
+    $("#trace_example3").click(function(e) {
+
+        e.preventDefault();
+
+        var inst1 = $('#example31').data('trace');
+        inst1.reTrace({
+            'stroke': '#880000',
+            'stroke-width': 2,
+            'stroke-opacity': 1,
+            'isVisible': 'true'
+        });
+
+        $('#example32').data('trace');
+        $('#example32').trigger({
+            type: 'adjust.trace',
+            adjustments: {
+                'stroke': '#aa0000',
+                'stroke-width': 4,
+                'stroke-opacity': 1
+            }
+        });
+
+    });
+
+    $("#trace_example4").click(function(e) {
+
+        e.preventDefault();
+
+        $('#example4').trace({
+            traceOpt: {
+                'stroke-width': 4,
+                'stroke-opacity': .5
+            },
+            onClick: function(me) {
+                alert('triggered when user clicks on a trace shape.');
+                me.options.shape.animate({
+                    opacity: 0
+                }, 1000, function() {
+                    me.hideTrace();
+                });
+            },
+            onHide: function() {
+                alert('triggered when hide animation completes.');
+            },
+            onEndTrace: function() {
+                alert("triggered when trace animation completes.");
+            },
+        });
+
+    });
+
 })(jQuery);
